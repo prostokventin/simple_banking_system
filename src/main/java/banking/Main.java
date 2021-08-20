b12110db22796373de78560a336134fd76da84fc
Ilia Ermakov <prostokventin2@gmail.com>
2021-08-20 03:34:09 +0300

    обернул в gradle проект

--- src/main/java/banking/Main.java
+++ src/main/java/banking/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String url = "jdbc:sqlite:" + args[1];
+        String url = "jdbc:sqlite:./card.s3db";
 
         DB.open(url);
 
